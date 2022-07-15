56d7a070a059e8f73f1e1e5e589561a1f635c4da
Muhammad Hermas Yuda Pamungkas <hermas.yuda@gmail.com>
2022-07-16 00:42:08 +0700

    rollback java

--- build.gradle.kts
+++ build.gradle.kts
@@ -42,7 +42,7 @@ dependencies {
 tasks.withType<KotlinCompile> {
     kotlinOptions {
         freeCompilerArgs = listOf("-Xjsr305=strict")
-        jvmTarget = "1.8"
+        jvmTarget = "11"
     }
 }
 
